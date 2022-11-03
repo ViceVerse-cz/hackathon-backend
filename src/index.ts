65216faaab6fdece064158c7f10f8c87f69e2f22
Atsukoro1 <dornicakkuba@gmail.com>
2022-11-03 21:53:06 +0100

    fix: 🐛 Dotenv instead of yaml

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 import Logger from '@ptkdev/logger';
 import mongoose from 'mongoose';
 import express from 'express';
-import fs from 'node:fs';
-import yaml from 'yaml';
+import dotenv from 'dotenv';
+
+dotenv.config({
+    path: './.env',
+});
 
 const LOGGER = new Logger();
-const CONFIG = yaml.parse(
-    fs.readFileSync('./config.yaml', 'utf8')
-);
+const PORT = parseInt(process.env.PORT || "3000");
 
 const app = express();
 
-mongoose.connect(CONFIG.DATABASE.uri)
-    .then(() => {
-        LOGGER.info('Connected to database');
-    })
-    .catch((err) => {
-        LOGGER.error(err);
-    });
+mongoose.connect(process.env.MONGO_URI || "")
+.then(() => {
+    LOGGER.info('Connected to database');
+})
+.catch((err) => {
+    LOGGER.error(err);
+});
 
-app.listen(CONFIG.HOST.port, CONFIG.HOST.hostname, () => {
-    LOGGER.info(`Server running at http://${CONFIG.HOST.hostname}:${CONFIG.HOST.port}/`);
+app.listen(PORT, "127.0.0.1", () => {
+    LOGGER.info(`Server running at http://127.0.0.1:${PORT}/`);
 });

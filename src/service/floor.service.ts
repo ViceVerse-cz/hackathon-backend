b0b805a24a36249bca80f21e8368bd87a6afbfc0
Atsukoro1 <dornicakkuba@gmail.com>
2022-11-04 12:40:09 +0100

    feat: 🎸 Push floor to building

--- src/service/floor.service.ts
+++ src/service/floor.service.ts
@@ -1,10 +1,18 @@
 import { Warehouse, WareHouseI } from "../schema/warehouse.schema";
+import { Building } from "../schema/building.schema";
 import { Shop, ShopI } from "../schema/shop.schema";
 import { Floor } from "../schema/floor.schema";
 import { Request, Response } from "express";
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
 
-export const createFloorService = (req: Request, res: Response) => {
+export const createFloorService = async (req: Request, res: Response) => {
+    // Find building and check if it exists
+    const foundBuilding = await Building.findById(req.body.building);
+    if(!foundBuilding) return res.status(404).json({
+        statusCode: 404,
+        message: "This building does not exist!",
+    });
+
     let newItem: WareHouseI & Document | ShopI & Document;
     if(req.body.type == "Warehouse") {
         newItem = new Warehouse({});
@@ -18,6 +26,9 @@ export const createFloorService = (req: Request, res: Response) => {
         object: newItem._id
     });
     newF.save();
+
+    foundBuilding.floors.push(new Types.ObjectId(newF._id));
+    foundBuilding.save();
 
     res.status(200).json({
         message: "Floor created successfully!",
